f9d10dbba9a4e9a6bd2e42c81543f3567b412fb6
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-11-27 00:08:30 +0300

    One to Many Relationship

---
+++ One_to_Many_Relationship/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace One_to_Many_Relationship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Default Convention
+            // -->Default convention yönteminde bire çok ilişkiyi kurarken foreign key kolonuna karşılık gelen bir property tanımlamak mecburiyetinde değilidiz. Eğer tanımlamazsak EF Core bunu kendisi tamamlayacak yok eğer tanımlarsak, tanımladığımızı baz alacaktır.
+            // --> Class Calisan //Dependent Entity
+            #endregion
+
+            #region Data Annotations
+            // --> Default convention yönteminde foreign key kolonuna karşılık gelen property'i tanımladığımızda bu property ismi temel geleneksel entity tanımlama kurallarına uymuyorsa eğer Data Annotations'lar ile müdahalede bulunabiliriz."
+            // --> Class Calisan //Dependent Entity
+            #endregion
+
+            #region Fluent API
+
+            #endregion
+        }
+    }
+}
